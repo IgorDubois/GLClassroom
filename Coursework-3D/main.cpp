f0a20cb98803bb52892f9331fe92e2f8bde584b1
Hippolyte Dubois <hippolytedubois@hippolytes-mbp.rgu.ac.uk>
2017-04-03 16:21:07 +0100

    Initial Commit

---
+++ Coursework-3D/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Coursework-3D
+//
+//  Created by Hippolyte Dubois on 03/04/17.
+//  Copyright © 2017 Hippolyte Dubois. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
